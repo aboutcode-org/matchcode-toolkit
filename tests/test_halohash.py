6911f4efc5aed68f64b02a85ca9f87b5d73f663f
Jono Yang <jyang@nexb.com>
2024-05-07 13:54:40 -0700

    Increase number of iterations per test
        * This is to get a better standard deviation
        * Refactor common code
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- tests/test_halohash.py
+++ tests/test_halohash.py
@@ -64,11 +64,43 @@ def check_results(
     for exp, res in zip(expected, results):
         assert exp[key1] == res[key1]
         expected_mean_hamming_distance = exp['mean hamming distance']
-        expected_standard_deviation = exp['standard deviation'] * 1.75
+        expected_standard_deviation = exp['standard deviation']
         exp_min = expected_mean_hamming_distance - expected_standard_deviation
         exp_max = expected_mean_hamming_distance + expected_standard_deviation
-        assert exp_min <= res['mean hamming distance']
-        assert res['mean hamming distance'] <= exp_max
+        assert exp_min <= res['mean hamming distance'] <= exp_max
+
+
+def calculate_similarity(content_hash, content_set, modified_content, number_of_words):
+    modified_content_hash = halohash.BitAverageHaloHash(modified_content)
+    hamming_distance = content_hash.distance(modified_content_hash)
+    fingerprint_similarity = ((SIZE_IN_BITS - hamming_distance) / SIZE_IN_BITS) * 100
+    input_similarity = (len(set(modified_content).intersection(content_set)) / number_of_words) * 100
+    return hamming_distance, fingerprint_similarity, input_similarity
+
+
+def calculate_mean_and_standard_deviation(hamming_distances):
+    """
+    Return a tuple of the mean hamming distance value and standard deviation from
+    a list of `hamming_distances`.
+    """
+    number_of_hamming_distances = len(hamming_distances)
+
+    # 1: Find the mean.
+    mean_hamming_distance = sum(hamming_distances) / number_of_hamming_distances
+
+    # 2: For each data point, find the square of its distance to the mean, then sum the values.
+    s0 = sum(
+        (hamming_distance - mean_hamming_distance) ** 2
+        for hamming_distance in hamming_distances
+    )
+
+    # 3: Divide by the number of data points.
+    s1 = s0 / number_of_hamming_distances
+
+    # 4: Take the square root.
+    standard_deviation = math.sqrt(s1)
+
+    return mean_hamming_distance, standard_deviation
 
 
 class TestHalohash(FileBasedTesting):
@@ -79,36 +111,30 @@ class TestHalohash(FileBasedTesting):
         with open(words_loc) as f:
             self.original_content = [bytes(x.strip(), 'utf-8') for x in f]
 
-    def calculate_similarity(self, original_hash, number_of_ngrams_original, content_hash, number_of_ngrams_content):
-        hamming_distance = original_hash.distance(content_hash)
-        fingerprint_similarity = ((SIZE_IN_BITS - hamming_distance) / SIZE_IN_BITS) * 100
-        # number of liens
-        input_similarity = (number_of_ngrams_content / number_of_ngrams_original) * 100
-        return fingerprint_similarity, input_similarity
-
-    # TODO: calculate standard deviation
     def test_halohash_random_delete(self, regen=False):
-        for number_of_words in [300, 1000]:
+        for number_of_words in [500, 1000]:
             content = copy.copy(self.original_content[:number_of_words])
+            content_set = set(content)
             original_hash = halohash.BitAverageHaloHash(content)
 
             hamming_distance_by_number_of_elements = defaultdict(list)
             fingerprint_similarity_by_number_of_elements = defaultdict(list)
             input_similarity_by_number_of_elements = defaultdict(list)
 
-            # Run test 10 times
-            for _ in range(10):
+            # Run test 30 times
+            for _ in range(30):
                 modified_content = copy.copy(content)
                 # Do not remove more than 10% of words for this test
                 # we are moving towards unrelated files past that
                 n = int(math.floor(len(modified_content) * 0.10))
                 for _ in range(n):
-                    h = halohash.BitAverageHaloHash(modified_content)
-                    number_of_elements = len(modified_content)
-                    hd = original_hash.distance(h)
-                    fingerprint_similarity = ((SIZE_IN_BITS - hd) / SIZE_IN_BITS) * 100
-                    input_similarity = (number_of_elements / number_of_words) * 100
-                    hamming_distance_by_number_of_elements[number_of_elements].append(hd)
+                    hamming_distance, fingerprint_similarity, input_similarity = calculate_similarity(
+                        original_hash,
+                        content_set,
+                        modified_content,
+                        number_of_words
+                    )
+                    hamming_distance_by_number_of_elements[number_of_elements].append(hamming_distance)
                     fingerprint_similarity_by_number_of_elements[number_of_elements].append(fingerprint_similarity)
                     input_similarity_by_number_of_elements[number_of_elements].append(input_similarity)
                     modified_content.pop(random.randint(0, len(modified_content) - 1))
@@ -116,23 +142,7 @@ class TestHalohash(FileBasedTesting):
             # Take mean and standard deviation
             results = []
             for number_of_elements, hamming_distances in hamming_distance_by_number_of_elements.items():
-                number_of_hamming_distances = len(hamming_distances)
-
-                # 1: Find the mean.
-                mean_hamming_distance = sum(hamming_distances) / number_of_hamming_distances
-
-                # 2: For each data point, find the square of its distance to the mean, then sum the values.
-                s0 = sum(
-                    (hamming_distance - mean_hamming_distance) ** 2
-                    for hamming_distance in hamming_distances
-                )
-
-                # 3: Divide by the number of data points.
-                s1 = s0 / number_of_hamming_distances
-
-                # 4: Take the square root.
-                standard_deviation = math.sqrt(s1)
-
+                mean_hamming_distance, standard_deviation = calculate_mean_and_standard_deviation(hamming_distances)
                 results.append(
                     {
                         'number of hashed elements': int(number_of_elements),
@@ -145,7 +155,7 @@ class TestHalohash(FileBasedTesting):
             check_results(results, expected_results_loc, ['number of hashed elements', 'mean hamming distance', 'standard deviation'], regen=regen)
 
     def test_halohash_random_replace(self, regen=False):
-        for number_of_words in [300, 1000]:
+        for number_of_words in [500, 1000]:
             content = copy.copy(self.original_content[:number_of_words])
             content_set = set(content)
             original_hash = halohash.BitAverageHaloHash(content)
@@ -154,23 +164,35 @@ class TestHalohash(FileBasedTesting):
             fingerprint_similarity_by_number_of_words_replaced = defaultdict(list)
             input_similarity_by_number_of_words_replaced = defaultdict(list)
 
-            # Run test 10 times
-            for _ in range(10):
+            # Run test 30 times
+            for _ in range(30):
                 modified_content = copy.copy(content)
                 words_replaced = 0
                 # Do not remove more than 10% of words for this test
                 # we are moving towards unrelated files past that
                 n = int(math.floor(len(modified_content) * 0.10))
                 for _ in range(n):
-                    h = halohash.BitAverageHaloHash(modified_content)
-                    hd = original_hash.distance(h)
-                    fingerprint_similarity = ((SIZE_IN_BITS - hd) / SIZE_IN_BITS) * 100
-                    input_similarity = (len(set(modified_content).intersection(content_set)) / number_of_words) * 100
-                    hamming_distance_by_number_of_words_replaced[words_replaced].append(hd)
+                    hamming_distance, fingerprint_similarity, input_similarity = calculate_similarity(
+                        original_hash,
+                        content_set,
+                        modified_content,
+                        number_of_words
+                    )
+                    hamming_distance_by_number_of_words_replaced[words_replaced].append(hamming_distance)
                     fingerprint_similarity_by_number_of_words_replaced[words_replaced].append(fingerprint_similarity)
                     input_similarity_by_number_of_words_replaced[words_replaced].append(input_similarity)
+
                     modified_content.pop(random.randint(0, len(modified_content) - 1))
-                    new_word = subprocess.run(['shuf', '-n', '1', '/usr/share/dict/american-english'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip().replace('"', '')
+                    new_word = (
+                        subprocess.run(
+                            ['shuf', '-n', '1', '/usr/share/dict/american-english'],
+                            stdout=subprocess.PIPE
+                        )
+                        .stdout
+                        .decode('utf-8')
+                        .strip()
+                        .replace('"', '')
+                    )
                     modified_content[random.randint(0, len(modified_content) - 1)] = bytes(new_word, 'utf-8')
                     words_replaced += 1
 
@@ -178,23 +200,7 @@ class TestHalohash(FileBasedTesting):
             # Take mean and standard deviation
             results = []
             for words_replaced, hamming_distances in hamming_distance_by_number_of_words_replaced.items():
-                number_of_hamming_distances = len(hamming_distances)
-
-                # 1: Find the mean.
-                mean_hamming_distance = sum(hamming_distances) / number_of_hamming_distances
-
-                # 2: For each data point, find the square of its distance to the mean, then sum the values.
-                s0 = sum(
-                    (hamming_distance - mean_hamming_distance) ** 2
-                    for hamming_distance in hamming_distances
-                )
-
-                # 3: Divide by the number of data points.
-                s1 = s0 / number_of_hamming_distances
-
-                # 4: Take the square root.
-                standard_deviation = math.sqrt(s1)
-
+                mean_hamming_distance, standard_deviation = calculate_mean_and_standard_deviation(hamming_distances)
                 results.append(
                     {
                         'words replaced': int(words_replaced),
